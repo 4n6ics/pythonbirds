6199048eacc51a7650e43cf42d4d69e457e20c7f
4n6ics <marsamb@gmail.com>
2021-06-14 15:31:07 -0300

    Criado e removido atributo dinamico de objeto o tipo Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -3,17 +3,23 @@ class Pessoa:
     def __init__(self,*filhos, nome, idade=42):    #eh um metodo de inicializacao de contrucao de objeto
         self.nome = nome                           #o atributo nome existe mas nao tem valor atribuido a ele
         self.idade = idade
-        self.filhos = list(filhos)
+        self.filhos = list(filhos)                 #lista contendo os nomes dos filhos
 
 
-    def cumprimentar (self):        #metodo cumprimentar
+    def cumprimentar (self):                        #metodo cumprimentar
         return 'Ola'
 
-if __name__ == '__main__':          #main eh usado para testar o cod dentro da classe
+if __name__ == '__main__':                          #main eh usado para testar o cod dentro da classe
     andre = Pessoa(nome='Andre')                    # Andre eh filho do Luciano
     luciano = Pessoa(andre, nome='Luciano')         #criacao o objeto p
     print (Pessoa.cumprimentar(andre))
     for filho in luciano.filhos:
         print (filho.nome)
 
+    andre.sobrenome = 'Silva'                       #cria um atributo dinamicamente para o objeto andre
+    print(andre.__dict__)                           #mostra todos dos os atributos de instancia do objeto
+    print(luciano.__dict__)
+
+    del andre.sobrenome                             #apaga um atributo do objeto
+    print(andre.__dict__)
 
